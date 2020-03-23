823513b146fcbc987b286e603ee733639a4f27fb
hsgilliland <manie.gilliland@gmail.com>
2020-03-23 23:39:44 +0200

    Fixed RadlistView For MainAdverts and MyAdverts Pages by using observable arrays

--- src/app/advert/my-adverts/my-advert.component.ts
+++ src/app/advert/my-adverts/my-advert.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { PageRoute, RouterExtensions } from "nativescript-angular/router";
 import { AdvertService } from "../advert.service";
-import {UserAdvertAccomodationResultList,UserAdvertNoteResultList,UserAdvertTextbookResultList,UserAdvertTutorResultList} from '../advert.model';
+import {UserAdvertAccomodationResultList,UserAdvertNoteResultList,UserAdvertTextbookResultList,UserAdvertTutorResultList, UserAdvertTextbookResult, UserAdvertAccomodationResult, UserAdvertNoteResult, UserAdvertTutorResult} from '../advert.model';
 import { Subscription } from "rxjs";
 import { TNSFancyAlert } from "nativescript-fancyalert";
 import * as appSettings from "tns-core-modules/application-settings";
 import { RadListView, ListViewEventData } from "nativescript-ui-listview";
 import { EventData } from "tns-core-modules/ui/page/page";
 import { Switch } from "tns-core-modules/ui/switch/switch";
+import { ObservableArray } from "tns-core-modules/data/observable-array/observable-array";
 @Component({
     selector: 'ns-my-adverts',
     templateUrl: './my-advert.component.html',
@@ -17,6 +18,11 @@ import { Switch } from "tns-core-modules/ui/switch/switch";
 
 
 export class MyAdvertComponent implements OnInit, OnDestroy {
+
+    public myTextbookArray : ObservableArray<UserAdvertTextbookResult>;
+    public myAccomodationArray : ObservableArray<UserAdvertAccomodationResult>;
+    public myNoteArray : ObservableArray<UserAdvertNoteResult>;
+    public myTutorArray : ObservableArray<UserAdvertTutorResult>;
 
     public UserID : string;
 
@@ -66,7 +72,6 @@ export class MyAdvertComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-        this.isSelling = appSettings.getBoolean("myAdvertSelling");
         this.textbookImagesLoaded = false;
         this.accomodationImagesLoaded = false;
         this.tutorImagesLoaded = false;
@@ -74,70 +79,78 @@ export class MyAdvertComponent implements OnInit, OnDestroy {
         this.allImagesLoaded = false;      
         
         this.userTextbookAdvertResultListSub = this.advertServ.currentUserAdvertTextbookList.subscribe(
-            userAdvertResult => {
-                if(userAdvertResult) {
-                    this.userTextbookAdvertResultList = userAdvertResult
-                    if(this.userTextbookAdvertResultList.responseStatusCode === 200){
+            textbookResult => {
+                if(textbookResult) {
+                    if(textbookResult.responseStatusCode === 200){
+                        this.myTextbookArray = new ObservableArray(0);
+                        textbookResult.Textbooks.forEach( t => {
+                            this.myTextbookArray.push(t);
+                        });
                         this.textbookImagesLoaded = true;
                         if(this.textbookImagesLoaded && this.accomodationImagesLoaded && this.tutorImagesLoaded && this.noteImagesLoaded){
                             this.allImagesLoaded = true;
                         }
-                    } else {
+                    }else {
                         TNSFancyAlert.showError("Data Retrieval", "Unable to retrieve data.");
                     }
-                    
                 }
             }
         );
 
 
         this.userAccomodationAdvertResultListSub = this.advertServ.currentUserAdvertAccomodationList.subscribe(
-            userAdvertResult => {
-                if(userAdvertResult) {
-                    this.userAccomodationAdvertResultList = userAdvertResult
-                    if(this.userAccomodationAdvertResultList.responseStatusCode === 200){
+            accomodationResult => {
+                if(accomodationResult) {
+                    if(accomodationResult.responseStatusCode === 200){
                         this.accomodationImagesLoaded = true;
                         if(this.textbookImagesLoaded && this.accomodationImagesLoaded && this.tutorImagesLoaded && this.noteImagesLoaded){
                             this.allImagesLoaded = true;
                         }
-                    } else {
+                    }else {
                         TNSFancyAlert.showError("Data Retrieval", "Unable to retrieve data.");
                     }
-                    
+                    this.myAccomodationArray = new ObservableArray(0);
+                    accomodationResult.Accomodations.forEach( t => {
+                        this.myAccomodationArray.push(t);
+                    })
                 }
             }
         );
 
         this.userTutorAdvertResultListSub = this.advertServ.currentUserAdvertTutorList.subscribe(
-            userAdvertResult => {
-                if(userAdvertResult) {
-                    this.userTutorAdvertResultList = userAdvertResult
-                    if(this.userTutorAdvertResultList.responseStatusCode === 200){
+            tutorResult => {
+                if(tutorResult) {
+                    if(tutorResult.responseStatusCode === 200){
                         this.tutorImagesLoaded = true;
                         if(this.textbookImagesLoaded && this.accomodationImagesLoaded && this.tutorImagesLoaded && this.noteImagesLoaded){
                             this.allImagesLoaded = true;
                         }
-                    } else {
+                    }else {
                         TNSFancyAlert.showError("Data Retrieval", "Unable to retrieve data.");
                     }
-                    
+                    this.myTutorArray = new ObservableArray(0);
+                    tutorResult.Tutors.forEach( t => {
+                        this.myTutorArray.push(t);
+                    })
                 }
             }
         );
 
         this.userNoteAdvertResultListSub = this.advertServ.currentUserAdvertNoteList.subscribe(
-            userAdvertResult => {
-                if(userAdvertResult) {
-                    this.userNoteAdvertResultList = userAdvertResult
-                    if(this.userNoteAdvertResultList.responseStatusCode === 200){
+            noteResult => {
+                if(noteResult) {
+                    if(noteResult.responseStatusCode === 200){
                         this.noteImagesLoaded = true;
                         if(this.textbookImagesLoaded && this.accomodationImagesLoaded && this.tutorImagesLoaded && this.noteImagesLoaded){
                             this.allImagesLoaded = true;
                         }
-                    } else {
+                    }else {
                         TNSFancyAlert.showError("Data Retrieval", "Unable to retrieve data.");
                     }
-                    
+                    this.myNoteArray = new ObservableArray(0);
+                    noteResult.Notes.forEach( t => {
+                        this.myNoteArray.push(t);
+                    })
                 }
             }
         );
