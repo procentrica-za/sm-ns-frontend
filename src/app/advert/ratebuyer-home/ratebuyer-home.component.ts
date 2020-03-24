d70cf2ef187cc41dcb623c4a0a2cafdee48621c3
hsgilliland <jo-ash@procentrica.co.za>
2020-03-24 14:31:18 +0200

    better styling

--- src/app/advert/ratebuyer-home/ratebuyer-home.component.ts
+++ src/app/advert/ratebuyer-home/ratebuyer-home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { AdvertService } from "../advert.service";
-import { InterestedbuyerResult, InterestedbuyerResultList} from '../advert.model';
+import { InterestedbuyerResult, InterestedbuyerResultList, RateBuyerResult} from '../advert.model';
 import { Subscription } from "rxjs";
 import { TNSFancyAlert } from "nativescript-fancyalert";
 import { RadListView, ListViewEventData } from "nativescript-ui-listview";
@@ -19,6 +19,9 @@ export class RatebuyerHomeComponent implements OnInit, OnDestroy {
     private interestedbuyerResultListSub: Subscription;
     public interestedbuyerResultList: InterestedbuyerResultList;
     public myInterestedbuyerArray : ObservableArray<InterestedbuyerResult>;
+     //Rating result for Tutoring
+     rateResultSub: Subscription;
+     rate: RateBuyerResult;
     constructor(private advertServ: AdvertService, private router: RouterExtensions) {
     }
     ngOnInit() {
@@ -44,6 +47,40 @@ export class RatebuyerHomeComponent implements OnInit, OnDestroy {
                 }
             }
         );
+
+        this.rateResultSub = this.advertServ.currentRateBuyer.subscribe( 
+            rateresult => {
+                if(rateresult){
+                    this.rate = rateresult;
+
+                    if(this.rate.responseStatusCode === 200 && this.rate.buyerrated === true){
+
+                       this.router.back();
+                       TNSFancyAlert.showSuccess("Rating Success", "The Student will now rate you.", "Dismiss")
+
+                    } else if (this.rate.responseStatusCode === 500 ){
+                        TNSFancyAlert.showError("Error Rating", this.rate.message, "Dismiss");
+                    }
+                    else if (this.rate.responseStatusCode === 200 && this.rate.ratingid === '00000000-0000-0000-0000-000000000000'){
+                        TNSFancyAlert.showError("Rating Already Completed.", this.rate.message, "Dismiss");
+                        this.router.navigate(['/advert/myadverts'],
+                  {
+                     animated: true,
+                     transition: {
+                     name: "slide",
+                     duration: 2,
+                     curve: "ease"
+                     }
+            });
+                    }
+                    else {
+                        TNSFancyAlert.showError("Error Rating", this.rate.message, "Dismiss");
+                        console.log(this.rate.buyerrated)
+                    }
+                }
+            }
+        );
+
         const userid = appSettings.getString("userid");
         const advertisementid = appSettings.getString("advertisementid");
         this.advertServ.InterestedBuyers(userid,advertisementid);
@@ -54,6 +91,14 @@ export class RatebuyerHomeComponent implements OnInit, OnDestroy {
         appSettings.setString("advertisementid", tappedInterestedItem.advertisementid);
         appSettings.setString("sellerid", tappedInterestedItem.sellerid);
         appSettings.setString("buyerid", tappedInterestedItem.buyerid);
+         //get to see if the advertisement is for a tutor
+         const advertisementtype = appSettings.getString("advertisementtype");
+         if (advertisementtype == 'TUT') {
+ 
+             this.advertServ.RateBuyer(tappedInterestedItem.advertisementid, tappedInterestedItem.sellerid, tappedInterestedItem.buyerid, '0', "Tutoring was concluded, a tutor may not rate a student");
+         }
+         else {
+ 
         this.router.navigate(['/ratebuyer'],
             {
                 animated: true,
@@ -63,12 +108,15 @@ export class RatebuyerHomeComponent implements OnInit, OnDestroy {
                     curve: "ease"
                 }
             });
-
+        }
     }
 
     ngOnDestroy() {
         if(this.interestedbuyerResultListSub){
             this.interestedbuyerResultListSub.unsubscribe();
+        }
+        if(this.rateResultSub){
+            this.rateResultSub.unsubscribe();
         }
     }
 }
