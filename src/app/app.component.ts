b483535ace94c466658d2f65e72d92d928994a1c
hsgilliland <jo-ash@procentrica.co.za>
2020-03-30 08:11:46 +0200

    Added unread messages to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit, ViewChild, ChangeDetectorRef } from "@angular/core";
-
+import { AdvertService } from "../app/advert/advert.service";
+import { UnreadChatsResult} from '../app/advert/advert.model';
+import { Subscription } from "rxjs";
+import { TNSFancyAlert } from "nativescript-fancyalert";
+import { ImageSource } from "tns-core-modules/image-source";
+import { RadListView, ListViewEventData } from "nativescript-ui-listview";
+import { RouterExtensions } from "nativescript-angular/router";
+import { EventData } from "tns-core-modules/ui/page/page";
+import { Switch } from "tns-core-modules/ui/switch/switch";
+import { ObservableArray, ChangedData } from "tns-core-modules/data/observable-array";
 
 import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular/side-drawer-directives";
 import { UIService } from './shared/ui/ui.service';
-import { RouterExtensions } from "nativescript-angular/router";
-
-import { Subscription } from 'rxjs'
 
 
 //Import for config file
@@ -22,15 +28,45 @@ export class AppComponent implements OnInit{
     private drawersub: Subscription;
     @ViewChild(RadSideDrawerComponent, {static: false}) drawerComponent: RadSideDrawerComponent;
 
-    
+     //unread messages
+     unreadchatsResultSub: Subscription;
+     unreadchats: UnreadChatsResult;
+     unreadMessages : boolean;
+
     constructor(
         private router: RouterExtensions,
         private uiService: UIService,
-        private changeDetectionRef: ChangeDetectorRef,){}
+        private changeDetectionRef: ChangeDetectorRef,
+        private advertServ: AdvertService){}
 
     ngOnInit(
     
-    ){}
+    ){
+        this.advertServ.UnreadChats();
+
+        this.unreadchatsResultSub = this.advertServ.currentUnreadMessages.subscribe(
+            unreadchatsresult => {
+                if(unreadchatsresult){
+                    this.unreadchats = unreadchatsresult;
+ 
+                    if(this.unreadchats.responseStatusCode === 200 && this.unreadchats.unreadmessages === true){
+                        this.unreadMessages = true;
+                
+                    } else if (this.unreadchats.responseStatusCode === 500){
+                        TNSFancyAlert.showError("Connection error", "An internal error has occured.", "Dismiss");
+                    }
+                    else if (this.unreadchats.responseStatusCode === 200 && this.unreadchats.unreadmessages === false){
+                        this.unreadMessages = false;
+          
+                    }
+                    else {
+                        TNSFancyAlert.showError("Error", "An Error has been recieved, please contact support." , "Dismiss");
+                    }
+                    
+                }
+            }
+        );
+    }
 
     onRouterOutletActivate(event : any) {
         if(appSettings.getBoolean("loggedIn")) {
@@ -67,17 +103,17 @@ export class AppComponent implements OnInit{
 
     Ratinghome(){
         this.drawerComponent.sideDrawer.closeDrawer();
-        this.router.navigate(['/rating']);
+        this.router.navigate(['ratinghome']);
     }
 
     Buyerhome(){
         this.drawerComponent.sideDrawer.closeDrawer();
-        this.router.navigate(['/rating/buyerrating']);
+        this.router.navigate(['buyerrating']);
     }
     
     Sellerhome(){
         this.drawerComponent.sideDrawer.closeDrawer();
-        this.router.navigate(['/rating/sellerrating']);
+        this.router.navigate(['sellerrating']);
     }
 
     logOut() {
@@ -86,5 +122,23 @@ export class AppComponent implements OnInit{
         appSettings.setBoolean("rememberme", false);
         appSettings.setBoolean("loggedIn", false);
         this.router.navigate([''], {clearHistory: true});
+    }
+    onViewUnread(){
+        this.router.navigate(['/messaginghome'],
+            {
+                animated: true,
+                transition: {
+                    name: "slide",
+                    duration: 200,
+                    curve: "ease"
+                }
+            });
+ 
+    }
+
+    ngOnDestroy() {
+        if(this.unreadchatsResultSub){
+            this.unreadchatsResultSub.unsubscribe();
+        }
     }
 }
