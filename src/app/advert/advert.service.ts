4295d19889c28a4342ebb5e7c3da88493cdfc73a
hsgilliland <jo-ash@procentrica.co.za>
2020-05-18 10:12:30 +0200

    new icons and splashscreen

--- src/app/advert/advert.service.ts
+++ src/app/advert/advert.service.ts
@@ -340,10 +340,10 @@ export class AdvertService {
    
 
     constructor(private http: HttpClient){
-        setString("sm-service-ratings-host", "http://192.168.1.187:9957");
-        setString("sm-service-advert-manager-host", "http://192.168.1.187:9953");
-        setString("sm-service-file-manager-host", "http://192.168.1.187:9955");
-        setString("sm-service-messages-host", "http://192.168.1.187:9956");
+        setString("sm-service-ratings-host", "http://192.168.1.188:9957");
+        setString("sm-service-advert-manager-host", "http://192.168.1.188:9953");
+        setString("sm-service-file-manager-host", "http://192.168.1.188:9955");
+        setString("sm-service-messages-host", "http://192.168.1.188:9956");
     }
     
     initializeModuleCodeList(){
